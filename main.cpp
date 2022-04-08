5489e86977c9f16a245c90c889d29d1e06f44a2e
Liangyue " Yue <liangyuezhao7@gmail.com>
2022-04-07 21:59:32 -0400

    Used the window class.

--- main.cpp
+++ main.cpp
@@ -1,31 +1,77 @@
-#include <GLFW/glfw3.h>
+#include <cstdlib>
+#include <cstring>
+#include <stdexcept>
+#include <iostream>
+
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+
+#include "window.hpp"
+
+class application_t
+{
+public:
+    application_t(): m_window(window_t::get_instance())
+    {
+        m_window.show();
+    }
+    
+    bool is_running()
+    {
+        return m_window.is_open();
+    }
+    
+    void update()
+    {
+        m_window.update();
+    }
+    
+    ~application_t()
+    {
+        
+    }
+    
+private:
+    window_t& m_window;
+};
+
+static void run()
+{
+    application_t application;
+    
+    while (application.is_running())
+    {
+        application.update();
+    }
+}
+
+static void display_fatal_error(const char* p_error_message)
+{
+    #ifdef _WIN32
+        size_t error_message_size = std::strlen(p_error_message);
+        wchar_t* error_message = new wchar_t[error_message_size];
+        mbstowcs_s(&error_message_size, error_message, error_message_size * 2, p_error_message, error_message_size - 1);
+        
+        MessageBoxW(nullptr, error_message, L"FATAL ERROR", MB_OK | MB_ICONERROR);
+        
+        delete[] error_message;
+    #else 
+        std::cerr << "[FATAL ERROR]: " << p_error_message << "\n";
+    #endif
+}
 
 int main()
 {
-    if (!glfwInit())
+    try
     {
-        return -1;
+        run();
+    }
+    catch(const std::runtime_error& e)
+    {
+        std::cerr << "[FATAL ERROR]: " << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
     
-    GLFWwindow* window = glfwCreateWindow(1280, 720, "Chess", nullptr, nullptr);
-    
-    if (!window)
-    {
-        glfwTerminate();
-        return -2;
-    }
-    
-    glfwMakeContextCurrent(window);
-    
-    while (!glfwWindowShouldClose(window))
-    {
-        glfwSwapBuffers(window);
-        
-        glfwPollEvents();
-    }
-    
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    
-    return 0;
+    return EXIT_SUCCESS;
 }
