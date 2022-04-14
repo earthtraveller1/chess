75390119647bacc75bded8b37aae976829fcbd31
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-04-14 15:59:03 -0400

    Made the borders thicker.

--- pieces.cpp
+++ pieces.cpp
@@ -4,7 +4,7 @@
 
 void pieces::draw_pawn(float p_x, float p_y)
 {
-    constexpr auto BORDER_WEIGHT { 0.05f };
+    constexpr auto BORDER_WEIGHT { 0.1f };
     
     renderer::draw_circle(p_x + .79f - BORDER_WEIGHT, p_y + .2f - BORDER_WEIGHT, .94f + BORDER_WEIGHT * 2, 0.0f, 0.0f, 0.0f, 1.0f);
     renderer::draw_colored_rectangle(p_x + .95f - BORDER_WEIGHT, p_y + .96f - BORDER_WEIGHT, .63f + BORDER_WEIGHT * 2, .86f + BORDER_WEIGHT * 2, 0.0f, 0.0f, 0.0f, 1.0f);
