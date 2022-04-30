30594ec366db709b593d45dcda8f06ef9ea94641
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-04-30 16:54:27 -0400

    Created a macro for unused parameters.

---
+++ utilities.hpp
@@ -0,0 +1,9 @@
+#ifndef AD37DA30_D0E5_4ABB_8835_9A59E058148C
+#define AD37DA30_D0E5_4ABB_8835_9A59E058148C
+
+// A set of useful utilities that I might use a lot.
+
+// Define a function parameter as unused.
+#define UNUSED_PARAM(x) (void)x
+
+#endif /* AD37DA30_D0E5_4ABB_8835_9A59E058148C */
