f524736f2d7ba4dac7a4c3fb4922626508d33de4
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-05-08 12:47:57 -0400

    Fixed a GL_INVALID_ENUM error.

--- renderer.cpp
+++ renderer.cpp
@@ -101,7 +101,7 @@ void renderer_t::set_texture(std::string_view p_path, uint8_t p_slot) noexcept
     glBindTexture(GL_TEXTURE_2D, texture);
     
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     
