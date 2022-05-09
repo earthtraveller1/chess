86140de3c752033015fcbe4e40353a72a372f5f3
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-05-08 20:28:50 -0400

    Upgraded to OpenGL 4.1.

--- window.cpp
+++ window.cpp
@@ -49,8 +49,8 @@ window_t::window_t()
     glfwSetErrorCallback(glfw_error_callback);
     
     #ifdef NDEBUG
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
         glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
         
     #else
