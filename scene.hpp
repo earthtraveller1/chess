2407d934b2d12585ee42114434ef2a6bc1375858
Liangyue "Yue" Zhao <liangyuezhao7@gmail.com>
2022-07-02 12:35:40 -0400

    Silenced unused parameters warning.

--- scene.hpp
+++ scene.hpp
@@ -1,6 +1,8 @@
 #ifndef E908B7A4_6FBD_42F8_8161_D6810EE55C2E
 #define E908B7A4_6FBD_42F8_8161_D6810EE55C2E
 
+#include "utilities.hpp"
+
 namespace chess
 {
     // An interface class that all scenes will inherit
@@ -13,9 +15,17 @@ namespace chess
         
         virtual void render() {};
         
-        virtual void on_mouse_click(int button, int action) {};
+        virtual void on_mouse_click(int button, int action)
+        {
+            UNUSED_PARAM(button);
+            UNUSED_PARAM(action);
+        }
         
-        virtual void on_mouse_move(double xpos, double ypos) {};
+        virtual void on_mouse_move(double xpos, double ypos) 
+        {
+            UNUSED_PARAM(xpos);
+            UNUSED_PARAM(ypos);
+        }
         
         virtual ~scene_t() {}
     };
